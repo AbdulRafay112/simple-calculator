2adfd6fffccec50b316c393420daa84fd6946e2a
AbdulRafay112 <rafayrafay922@gmail.com>
2024-03-22 00:40:20 -0700

    your is added in the file

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@
 // jb hamara project bn raha hota hai to is duraan hamein dev dependencies ki zororat prhti hai lekin jb project bnjtata hai to is ki zarorat nhi prhti
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "enter first number", type: "number", name: "firstNumber" },
-  {message: "enter second number", type: "number", name: "secondNumber"},
+  { message: "enter your first number", type: "number", name: "firstNumber" },
+  {message: "enter your second number", type: "number", name: "secondNumber"},
   {message: "Select one of the operators to perform operation" , type: "list" , name: "operator" , choices:["Addition" , "Substraction" , "Multiplication" , "Division"] }
 ]);
 
